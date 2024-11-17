a5a2d825a184ac6186b2031d1fc235a947f0dfba
Karam Khan <karamkhan0258@gmail.com>
2024-11-17 01:00:04 +0000

    tensor conversions

--- cnn_model.py
+++ cnn_model.py
@@ -68,7 +68,12 @@ X_train_scaled = scaler.fit_transform(X_train)
 X_val_scaled = scaler.transform(X_val)
 X_test_scaled = scaler.transform(test_data.drop(columns=['isic_id', 'patient_id']))
 
-
+#Tensor conversions
+X_train_tensor = torch.tensor(X_train_scaled, dtype=torch.float32)
+y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
+X_val_tensor = torch.tensor(X_val_scaled, dtype=torch.float32)
+y_val_tensor = torch.tensor(y_val.values, dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test_scaled, dtype=torch.float32)
 
 
 
